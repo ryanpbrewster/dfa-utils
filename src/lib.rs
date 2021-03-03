55a3cc5f56a5b3c08842324ea4955efc6b9b29de
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-03-02 19:23:48 -0500

    try rewriting

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,148 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
+#[derive(Debug)]
 struct DFA {
-    // states[0] is the initial state
-    states: Vec<State>,
+    num_states: usize,
+    transitions: Vec<Transition>,
+    initial_state: u32,
+    final_states: HashSet<u32>,
 }
-struct State {
-    accepting: bool,
-    transitions: HashMap<u8, usize>,
+#[derive(Debug, Clone, Copy)]
+struct Transition {
+    from: u32,
+    label: u8,
+    to: u32,
 }
 
-type Label = usize;
-struct Partition {
-    //The number of sets in the partition
-    z: usize,
-    // The elements themselves.
-    elements: Vec<usize>,
-    // The start index of the elements of set `i` in `elements`.
-    first: Vec<usize>,
-    // Just past the last index of the elements of set `i` in `elements`.
-    past: Vec<usize>,
-    // The location of element `e` in `elements`.
-    location: Vec<usize>,
+fn prune_unreachable(input: &DFA) -> DFA {
+    let mut outflows: HashMap<u32, HashSet<u32>> = HashMap::new();
+    let mut inflows: HashMap<u32, HashSet<u32>> = HashMap::new();
+    for t in &input.transitions {
+        outflows.entry(t.from).or_default().insert(t.to);
+        inflows.entry(t.to).or_default().insert(t.from);
+    }
+
+    let mut reachable = HashSet::new();
+    let mut queue = VecDeque::new();
+    queue.push_back(input.initial_state);
+    while let Some(q) = queue.pop_front() {
+        if !reachable.insert(q) {
+            continue;
+        }
+        if let Some(neighbors) = outflows.get(&q) {
+            for &qq in neighbors {
+                queue.push_back(qq);
+            }
+        }
+    }
+
+    let mut relevant = HashSet::new();
+    for &q in &input.final_states {
+        queue.push_back(q);
+    }
+    while let Some(q) = queue.pop_front() {
+        if !relevant.insert(q) {
+            continue;
+        }
+        if let Some(neighbors) = inflows.get(&q) {
+            for &qq in neighbors {
+                queue.push_back(qq);
+            }
+        }
+    }
+
+    let allowed: HashSet<u32> = reachable.intersection(&relevant).copied().collect();
+    assert!(allowed.contains(&input.initial_state));
+
+    DFA {
+        num_states: allowed.len(),
+        initial_state: input.initial_state,
+        final_states: input.final_states.intersection(&allowed).copied().collect(),
+        transitions: input
+            .transitions
+            .iter()
+            .filter(|t| allowed.contains(&t.from) && allowed.contains(&t.to))
+            .copied()
+            .collect(),
+    }
 }
+
+fn minimize(input: DFA) {}
 
 #[cfg(test)]
-mod tests {
-    use super::{State, DFA};
+mod test {
+    use super::*;
+
     #[test]
-    fn minimize_1() {
-        let a = State {
-            accepting: false,
-            transitions: vec![(0, 1), (1, 2)].into_iter().collect(),
-        };
-        let b = State {
-            accepting: false,
-            transitions: vec![(0, 0), (1, 3)].into_iter().collect(),
-        };
-        let c = State {
-            accepting: true,
-            transitions: vec![(0, 4), (1, 5)].into_iter().collect(),
-        };
-        let d = State {
-            accepting: true,
-            transitions: vec![(0, 4), (1, 5)].into_iter().collect(),
-        };
-        let e = State {
-            accepting: true,
-            transitions: vec![(0, 4), (1, 5)].into_iter().collect(),
-        };
-        let f = State {
-            accepting: false,
-            transitions: vec![(0, 5), (1, 5)].into_iter().collect(),
-        };
+    fn wikipedia() {
         let input = DFA {
-            states: vec![a, b, c, d, e, f],
+            initial_state: 0,
+            final_states: vec![2, 3, 4].into_iter().collect(),
+            num_states: 6,
+            transitions: vec![
+                Transition {
+                    from: 0,
+                    to: 1,
+                    label: 0,
+                },
+                Transition {
+                    from: 0,
+                    to: 2,
+                    label: 1,
+                },
+                Transition {
+                    from: 1,
+                    to: 0,
+                    label: 0,
+                },
+                Transition {
+                    from: 1,
+                    to: 3,
+                    label: 1,
+                },
+                Transition {
+                    from: 2,
+                    to: 4,
+                    label: 0,
+                },
+                Transition {
+                    from: 2,
+                    to: 5,
+                    label: 1,
+                },
+                Transition {
+                    from: 3,
+                    to: 4,
+                    label: 0,
+                },
+                Transition {
+                    from: 3,
+                    to: 5,
+                    label: 1,
+                },
+                Transition {
+                    from: 4,
+                    to: 4,
+                    label: 0,
+                },
+                Transition {
+                    from: 4,
+                    to: 5,
+                    label: 1,
+                },
+                Transition {
+                    from: 5,
+                    to: 5,
+                    label: 0,
+                },
+                Transition {
+                    from: 5,
+                    to: 5,
+                    label: 1,
+                },
+            ],
         };
-        let output = minimize(input);
-        assert_eq!(output.states.len(), 3);
+        println!("{:?}", input);
+        println!("{:?}", prune_unreachable(&input));
     }
 }
