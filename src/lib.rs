a6a52f1f52697f942bb726d28f154e210b0c2ec2
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-03-02 22:11:51 -0500

    introduce table

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,69 @@
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::Hash;
+
+use table::Table;
+
+mod table;
 
 #[derive(Debug)]
-struct DFA {
-    num_states: usize,
-    transitions: Vec<Transition>,
-    initial_state: u32,
-    final_states: HashSet<u32>,
-}
-#[derive(Debug, Clone, Copy)]
-struct Transition {
-    from: u32,
-    label: u8,
-    to: u32,
+struct DFA<S, E> {
+    initial_state: S,
+    final_states: HashSet<S>,
+    transitions: Table<S, E, S>,
 }
 
-fn prune_unreachable(input: &DFA) -> DFA {
-    let mut outflows: HashMap<u32, HashSet<u32>> = HashMap::new();
-    let mut inflows: HashMap<u32, HashSet<u32>> = HashMap::new();
-    for t in &input.transitions {
-        outflows.entry(t.from).or_default().insert(t.to);
-        inflows.entry(t.to).or_default().insert(t.from);
-    }
+impl<S, E> DFA<S, E>
+where
+    S: Eq + Hash + Copy,
+    E: Eq + Hash + Copy,
+{
+    fn prune_unreachable(self) -> DFA<S, E> {
+        let mut outflows = self.transitions.by_a();
+        let mut inflows = self.transitions.by_c();
 
-    let mut reachable = HashSet::new();
-    let mut queue = VecDeque::new();
-    queue.push_back(input.initial_state);
-    while let Some(q) = queue.pop_front() {
-        if !reachable.insert(q) {
-            continue;
-        }
-        if let Some(neighbors) = outflows.get(&q) {
-            for &qq in neighbors {
-                queue.push_back(qq);
+        let mut reachable = HashSet::new();
+        let mut queue = VecDeque::new();
+        queue.push_back(self.initial_state);
+        while let Some(src) = queue.pop_front() {
+            if !reachable.insert(src) {
+                continue;
+            }
+            if let Some(neighbors) = outflows.get(&src) {
+                for &(_, dst) in neighbors {
+                    queue.push_back(dst);
+                }
             }
         }
-    }
 
-    let mut relevant = HashSet::new();
-    for &q in &input.final_states {
-        queue.push_back(q);
-    }
-    while let Some(q) = queue.pop_front() {
-        if !relevant.insert(q) {
-            continue;
+        let mut relevant = HashSet::new();
+        for &q in &self.final_states {
+            queue.push_back(q);
         }
-        if let Some(neighbors) = inflows.get(&q) {
-            for &qq in neighbors {
-                queue.push_back(qq);
+        while let Some(dst) = queue.pop_front() {
+            if !relevant.insert(dst) {
+                continue;
+            }
+            if let Some(neighbors) = inflows.get(&dst) {
+                for &(src, _) in neighbors {
+                    queue.push_back(src);
+                }
             }
         }
-    }
 
-    let allowed: HashSet<u32> = reachable.intersection(&relevant).copied().collect();
-    assert!(allowed.contains(&input.initial_state));
+        let allowed: HashSet<S> = reachable.intersection(&relevant).copied().collect();
+        assert!(allowed.contains(&self.initial_state));
 
-    DFA {
-        num_states: allowed.len(),
-        initial_state: input.initial_state,
-        final_states: input.final_states.intersection(&allowed).copied().collect(),
-        transitions: input
-            .transitions
-            .iter()
-            .filter(|t| allowed.contains(&t.from) && allowed.contains(&t.to))
-            .copied()
-            .collect(),
+        DFA {
+            initial_state: self.initial_state,
+            final_states: self.final_states.intersection(&allowed).copied().collect(),
+            transitions: self
+                .transitions
+                .into_iter()
+                .filter(|(src, _, dst)| allowed.contains(src) && allowed.contains(dst))
+                .collect(),
+        }
     }
 }
-
-fn minimize(input: DFA) {}
 
 #[cfg(test)]
 mod test {
@@ -75,74 +71,26 @@ mod test {
 
     #[test]
     fn wikipedia() {
-        let input = DFA {
+        let transitions: Vec<(u32, u8, u32)> = vec![
+            (0, 0, 1),
+            (0, 1, 2),
+            (1, 0, 0),
+            (1, 1, 3),
+            (2, 0, 4),
+            (2, 1, 5),
+            (3, 0, 4),
+            (3, 1, 5),
+            (4, 0, 4),
+            (4, 1, 5),
+            (5, 0, 5),
+            (5, 1, 5),
+        ];
+        let input: DFA<u32, u8> = DFA {
             initial_state: 0,
             final_states: vec![2, 3, 4].into_iter().collect(),
-            num_states: 6,
-            transitions: vec![
-                Transition {
-                    from: 0,
-                    to: 1,
-                    label: 0,
-                },
-                Transition {
-                    from: 0,
-                    to: 2,
-                    label: 1,
-                },
-                Transition {
-                    from: 1,
-                    to: 0,
-                    label: 0,
-                },
-                Transition {
-                    from: 1,
-                    to: 3,
-                    label: 1,
-                },
-                Transition {
-                    from: 2,
-                    to: 4,
-                    label: 0,
-                },
-                Transition {
-                    from: 2,
-                    to: 5,
-                    label: 1,
-                },
-                Transition {
-                    from: 3,
-                    to: 4,
-                    label: 0,
-                },
-                Transition {
-                    from: 3,
-                    to: 5,
-                    label: 1,
-                },
-                Transition {
-                    from: 4,
-                    to: 4,
-                    label: 0,
-                },
-                Transition {
-                    from: 4,
-                    to: 5,
-                    label: 1,
-                },
-                Transition {
-                    from: 5,
-                    to: 5,
-                    label: 0,
-                },
-                Transition {
-                    from: 5,
-                    to: 5,
-                    label: 1,
-                },
-            ],
+            transitions: Table::from(transitions),
         };
         println!("{:?}", input);
-        println!("{:?}", prune_unreachable(&input));
+        println!("{:?}", input.prune_unreachable());
     }
 }
---
+++ src/table.rs
@@ -0,0 +1,58 @@
+use std::{collections::HashMap, hash::Hash, iter::FromIterator};
+
+#[derive(Debug)]
+pub struct Table<A, B, C> {
+    tuples: Vec<(A, B, C)>,
+}
+impl<A, B, C> From<Vec<(A, B, C)>> for Table<A, B, C> {
+    fn from(tuples: Vec<(A, B, C)>) -> Self {
+        Table { tuples }
+    }
+}
+impl<A, B, C> IntoIterator for Table<A, B, C> {
+    type Item = (A, B, C);
+
+    type IntoIter = std::vec::IntoIter<(A, B, C)>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tuples.into_iter()
+    }
+}
+impl<A, B, C> FromIterator<(A, B, C)> for Table<A, B, C> {
+    fn from_iter<T: IntoIterator<Item = (A, B, C)>>(iter: T) -> Self {
+        Table {
+            tuples: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl<A, B, C> Table<A, B, C>
+where
+    A: Eq + Hash + Copy,
+    B: Eq + Hash + Copy,
+    C: Eq + Hash + Copy,
+{
+    pub fn by_a(&self) -> HashMap<A, Vec<(B, C)>> {
+        group_by_to(&self.tuples, |&(a, _, _)| a, |&(_, b, c)| (b, c))
+    }
+    pub fn by_b(&self) -> HashMap<B, Vec<(A, C)>> {
+        group_by_to(&self.tuples, |&(_, b, _)| b, |&(a, _, c)| (a, c))
+    }
+    pub fn by_c(&self) -> HashMap<C, Vec<(A, B)>> {
+        group_by_to(&self.tuples, |&(_, _, c)| c, |&(a, b, _)| (a, b))
+    }
+}
+
+fn group_by_to<T, K: Eq + Hash, V>(
+    input: &[T],
+    key_fn: impl Fn(&T) -> K,
+    value_fn: impl Fn(&T) -> V,
+) -> HashMap<K, Vec<V>> {
+    let mut output: HashMap<K, Vec<V>> = HashMap::new();
+    for t in input {
+        let k = key_fn(t);
+        let v = value_fn(t);
+        output.entry(k).or_default().push(v);
+    }
+    output
+}
